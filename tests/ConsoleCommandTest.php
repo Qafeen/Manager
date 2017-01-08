113926578876a1d5f43ff6d42b5dee2233901cce
Mohammed Mudasir <md.hyphen@gmail.com>
2017-01-09 00:50:34 +0530

    Added test cases.

---
+++ tests/ConsoleCommandTest.php
@@ -0,0 +1,34 @@
+<?php
+use Mockery as m;
+
+class ConsoleCommandTest extends PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testAddConsoleCommand()
+    {
+        $app = $this->getMockConsole(['addToParent']);
+        $command = m::mock('Qafeen\Manager\Console\Add');
+        $command->shouldReceive('setLaravel')->once()->with(m::type('Illuminate\Contracts\Foundation\Application'));
+        $app->expects($this->once())->method('addToParent')->with($this->equalTo($command))->will($this->returnValue($command));
+
+        $result = $app->add($command);
+
+        $this->assertEquals($command, $result);
+    }
+
+    protected function getMockConsole(array $methods)
+    {
+        $app = m::mock('Illuminate\Contracts\Foundation\Application', ['version' => '5.3']);
+        $events = m::mock('Illuminate\Contracts\Events\Dispatcher', ['fire' => null]);
+
+        $console = $this->getMockBuilder('Illuminate\Console\Application')->setMethods($methods)->setConstructorArgs([
+            $app, $events, 'test-version',
+        ])->getMock();
+
+        return $console;
+    }
+}
---
+++ tests/SearchTest.php
@@ -0,0 +1,62 @@
+<?php
+use Mockery as m;
+use Qafeen\Manager\Manage\Migration;
+use Qafeen\Manager\Manage\Facade;
+use Qafeen\Manager\Manage\ServiceProvider;
+use Qafeen\Manager\Packages;
+use Symfony\Component\Finder\Finder;
+
+class SearchTest extends PHPUnit_Framework_TestCase
+{
+    public function test_packages_search()
+    {
+        $result = Packages::instance('laravel/laravel')->search()->first()['name'];
+
+        $this->assertEquals('laravel/laravel', $result);
+    }
+
+    public function test_facades_search()
+    {
+        $console = $this->mockAddConsoleCommand();
+
+        $finder = Finder::create()->in(__DIR__.DIRECTORY_SEPARATOR.'dummy');
+
+        $result = Facade::instance($finder, $console)->search();
+
+        $this->assertEquals([], $result);
+    }
+
+    public function test_provider_search()
+    {
+        $console = $this->mockAddConsoleCommand();
+
+        $finder = Finder::create()->in(__DIR__.DIRECTORY_SEPARATOR.'dummy');
+
+        $result = ServiceProvider::instance($finder, $console)->search();
+
+        $this->assertEquals([], $result);
+    }
+
+    public function test_migration_search()
+    {
+        $console = $this->mockAddConsoleCommand();
+
+        $finder = Finder::create()->in(__DIR__.DIRECTORY_SEPARATOR.'dummy');
+
+        $result = Migration::instance($finder, $console)->run();
+
+        $this->assertEquals(false, $result);
+    }
+
+    protected function mockAddConsoleCommand()
+    {
+        return $this->getMockBuilder('\Qafeen\Manager\Console\Add')
+                ->setMethods(['info', 'error', 'line', 'confirm', 'warn'])
+                ->getMock();
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+}
